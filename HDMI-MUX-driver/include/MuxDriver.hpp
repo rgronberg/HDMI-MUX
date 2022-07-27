423de28f0132db32d348c15d704b0964347faaa7
Rion Gronberg <rion.gronberg@gmail.com>
2022-07-26 21:45:20 -0700

    Complete MuxDriver class

--- HDMI-MUX-driver/include/MuxDriver.hpp
+++ HDMI-MUX-driver/include/MuxDriver.hpp
@@ -5,10 +5,61 @@
 #ifndef __MUXDRIVER_HPP__
 #define __MUXDRIVER_HPP__
 
+#include <Arduino.h>
+
+// Forward definition of MuxDriver class for use in EnableFuncPtr
+class MuxDriver;
+typedef void (MuxDriver::*EnableFuncPtr)();
+
 class MuxDriver
 {
+    private:
+        /***** Member constants *****/
+        const unsigned long debounceDelay = 50; // ms
+        const int numPorts = 4;                 // Number of ports in the mux
+        const EnableFuncPtr enableFunctions[4] = { &MuxDriver::enablePort1,
+                                                   &MuxDriver::enablePort2,
+                                                   &MuxDriver::enablePort3,
+                                                   &MuxDriver::enablePort4 };
+
+        /*** Inputs ***/
+        const int buttonPin = 16;
+
+        /*** Outputs ***/
+        // The following enable/select pins are low until the code takes them high.
+        const int muxA_enable = 5;
+        const int muxA_select = 12;
+        const int muxB_enable = 13;
+        const int muxB_select = 14;
+
+        // GPIO 0, 2, and 15 are used for boot functions and are not used to drive the MUX logic.
+        const int port1LED = 0;
+        const int port2LED = 2;
+        const int port3LED = 4;
+        const int port4LED = 15;
+
+        /***** Member variables *****/
+        unsigned long lastDebounceTime = 0; // the last time the input pin was toggled
+        int buttonState = HIGH;             // the current reading from the input pin
+        int lastButtonState = HIGH;         // the previous reading from the input pin
+        int currentPortIndex = 0;           // the currently sellected mux port (zero based)
+
+        /***** Internal methods *****/
+        void clearEnabledPort();
+
     public:
-        MuxDriver();
+        /***** Setup and loop functions *****/
+        void begin(int initialPort);
+        void handleButton();
+
+        /***** State management functions *****/
+        void enablePort1();
+        void enablePort2();
+        void enablePort3();
+        void enablePort4();
+        void enablePort(int port);  // One-based port index
+        void enableNextPort();
+        int enabledPort();          // One-based port index
 };
 
-#endif
+#endif  // __MUXDRIVER_HPP__
--- HDMI-MUX-driver/src/MuxDriver.cpp
+++ HDMI-MUX-driver/src/MuxDriver.cpp
@@ -4,5 +4,109 @@
 
 #include "MuxDriver.hpp"
 
-MuxDriver::MuxDriver() {
+void MuxDriver::begin(int initialPort /* one-based */) {
+    // Set the pin modes for the pins used in the class
+    pinMode(this->buttonPin, INPUT);
+    pinMode(this->muxA_enable, OUTPUT);
+    pinMode(this->muxA_select, OUTPUT);
+    pinMode(this->muxB_enable, OUTPUT);
+    pinMode(this->muxB_select, OUTPUT);
+    pinMode(this->port1LED, OUTPUT);
+    pinMode(this->port2LED, OUTPUT);
+    pinMode(this->port3LED, OUTPUT);
+    pinMode(this->port4LED, OUTPUT);
+
+    // Set initial state
+    this->enablePort(initialPort);
+}
+
+void MuxDriver::handleButton() {
+    // read the state of the switch into a local variable:
+    int reading = digitalRead(this->buttonPin);
+
+    // check to see if you just pressed the button
+    // (i.e. the input went from LOW to HIGH), and you've waited long enough
+    // since the last press to ignore any noise:
+
+    // If the switch changed, due to noise or pressing:
+    if (reading != this->lastButtonState) {
+        // reset the debouncing timer
+        this->lastDebounceTime = millis();
+    }
+
+    if ((millis() - this->lastDebounceTime) > this->debounceDelay) {
+        // whatever the reading is at, it's been there for longer than the debounce
+        // delay, so take it as the actual current state:
+
+        // if the button state has changed:
+        if (reading != this->buttonState) {
+            this->buttonState = reading;
+
+            // Only toggle the LED if the new button state is HIGH
+            // This performs the action when the button is unpressed
+            if (this->buttonState == HIGH) {
+                // Enable the next port
+                this->enableNextPort();
+            }
+        }
+    }
+
+    // save the reading for the next time through the loop
+    this->lastButtonState = reading;
+}
+
+void MuxDriver::clearEnabledPort() {
+    digitalWrite(muxA_enable, LOW);
+    digitalWrite(muxA_select, LOW);
+    digitalWrite(muxB_enable, LOW);
+    digitalWrite(muxB_select, LOW);
+    digitalWrite(port1LED, LOW);
+    digitalWrite(port2LED, LOW);
+    digitalWrite(port3LED, LOW);
+    digitalWrite(port4LED, LOW);
+}
+
+void MuxDriver::enablePort1() {
+    this->clearEnabledPort();
+    digitalWrite(muxA_enable, HIGH);
+    digitalWrite(muxA_select, LOW);
+    digitalWrite(port1LED, HIGH);
+}
+
+void MuxDriver::enablePort2() {
+    this->clearEnabledPort();
+    digitalWrite(muxA_enable, HIGH);
+    digitalWrite(muxA_select, HIGH);
+    digitalWrite(port2LED, HIGH);
+}
+
+void MuxDriver::enablePort3() {
+    this->clearEnabledPort();
+    digitalWrite(muxB_enable, HIGH);
+    digitalWrite(muxB_select, LOW);
+    digitalWrite(port3LED, HIGH);
+}
+
+void MuxDriver::enablePort4() {
+    this->clearEnabledPort();
+    digitalWrite(muxB_enable, HIGH);
+    digitalWrite(muxB_select, HIGH);
+    digitalWrite(port4LED, HIGH);
+}
+
+void MuxDriver::enablePort(int port /* one-based */) {
+    if (0 < port && port <= this->numPorts) {
+        this->currentPortIndex = port - 1;
+        (this->*(this->enableFunctions[this->currentPortIndex]))();
+    }
+}
+
+void MuxDriver::enableNextPort() {
+    int newPortOneBased = ((this->currentPortIndex + 1) % this->numPorts) + 1;
+    this->enablePort(newPortOneBased);
+}
+
+int MuxDriver::enabledPort() {
+    // Return value is one-based
+    return this->currentPortIndex + 1;
 }
