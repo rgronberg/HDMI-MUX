cb02f8e76835c043a320bcd50ac98d3774dd4a11
Rion Gronberg <rion.gronberg@gmail.com>
2022-07-31 11:37:54 -0700

    Cleanup in MuxDriver class

--- HDMI-MUX-driver/src/MuxDriver.cpp
+++ HDMI-MUX-driver/src/MuxDriver.cpp
@@ -6,60 +6,60 @@
 
 void MuxDriver::begin(int initialPort /* one-based */) {
     // Set the pin modes for the pins used in the class
-    pinMode(this->buttonPin, INPUT);
-    pinMode(this->muxA_enable, OUTPUT);
-    pinMode(this->muxA_select, OUTPUT);
-    pinMode(this->muxB_enable, OUTPUT);
-    pinMode(this->muxB_select, OUTPUT);
-    pinMode(this->port1LED, OUTPUT);
-    pinMode(this->port2LED, OUTPUT);
-    pinMode(this->port3LED, OUTPUT);
-    pinMode(this->port4LED, OUTPUT);
+    pinMode(buttonPin, INPUT);
+    pinMode(muxA_enable, OUTPUT);
+    pinMode(muxA_select, OUTPUT);
+    pinMode(muxB_enable, OUTPUT);
+    pinMode(muxB_select, OUTPUT);
+    pinMode(port1LED, OUTPUT);
+    pinMode(port2LED, OUTPUT);
+    pinMode(port3LED, OUTPUT);
+    pinMode(port4LED, OUTPUT);
 
     // Set initial state
-    this->enablePort(initialPort);
+    enablePort(initialPort);
 }
 
 void MuxDriver::handleButton() {
     // read the state of the switch into a local variable:
-    int reading = digitalRead(this->buttonPin);
+    int reading = digitalRead(buttonPin);
 
     // check to see if you just pressed the button
     // (i.e. the input went from LOW to HIGH), and you've waited long enough
     // since the last press to ignore any noise:
 
     // If the switch changed, due to noise or pressing:
-    if (reading != this->lastButtonState) {
+    if (reading != lastButtonState) {
         // reset the debouncing timer
-        this->lastDebounceTime = millis();
+        lastDebounceTime = millis();
     }
 
     // Long button press should cause settings reset
-    if ((millis() - this->lastDebounceTime) > this->longPress &&
-            this->buttonState == LOW &&
-            !this->_shouldReset) {
-        this->_shouldReset = true;
+    if ((millis() - lastDebounceTime) > longPress &&
+            buttonState == LOW &&
+            !_shouldReset) {
+        _shouldReset = true;
     }
 
-    if ((millis() - this->lastDebounceTime) > this->debounceDelay) {
+    if ((millis() - lastDebounceTime) > debounceDelay) {
         // whatever the reading is at, it's been there for longer than the debounce
         // delay, so take it as the actual current state:
 
         // if the button state has changed:
-        if (reading != this->buttonState) {
-            this->buttonState = reading;
+        if (reading != buttonState) {
+            buttonState = reading;
 
             // Only toggle the LED if the new button state is HIGH
             // This performs the action when the button is unpressed
-            if (this->buttonState == HIGH) {
+            if (buttonState == HIGH) {
                 // Enable the next port
-                this->enableNextPort();
+                enableNextPort();
             }
         }
     }
 
     // save the reading for the next time through the loop
-    this->lastButtonState = reading;
+    lastButtonState = reading;
 }
 
 void MuxDriver::clearEnabledPort() {
@@ -74,54 +74,54 @@ void MuxDriver::clearEnabledPort() {
 }
 
 void MuxDriver::enablePort1() {
-    this->clearEnabledPort();
+    clearEnabledPort();
     digitalWrite(muxA_enable, HIGH);
     digitalWrite(muxA_select, LOW);
     digitalWrite(port1LED, HIGH);
-    this->currentPortIndex = 0;
+    currentPortIndex = 0;
 }
 
 void MuxDriver::enablePort2() {
-    this->clearEnabledPort();
+    clearEnabledPort();
     digitalWrite(muxA_enable, HIGH);
     digitalWrite(muxA_select, HIGH);
     digitalWrite(port2LED, HIGH);
-    this->currentPortIndex = 1;
+    currentPortIndex = 1;
 }
 
 void MuxDriver::enablePort3() {
-    this->clearEnabledPort();
+    clearEnabledPort();
     digitalWrite(muxB_enable, HIGH);
     digitalWrite(muxB_select, LOW);
     digitalWrite(port3LED, HIGH);
-    this->currentPortIndex = 2;
+    currentPortIndex = 2;
 }
 
 void MuxDriver::enablePort4() {
-    this->clearEnabledPort();
+    clearEnabledPort();
     digitalWrite(muxB_enable, HIGH);
     digitalWrite(muxB_select, HIGH);
     digitalWrite(port4LED, HIGH);
-    this->currentPortIndex = 3;
+    currentPortIndex = 3;
 }
 
 void MuxDriver::enablePort(int port /* one-based */) {
-    if (0 < port && port <= this->numPorts) {
-        this->currentPortIndex = port - 1;
-        (this->*(this->enableFunctions[this->currentPortIndex]))();
+    if (0 < port && port <= numPorts) {
+        currentPortIndex = port - 1;
+        (this->*(enableFunctions[currentPortIndex]))();
     }
 }
 
 void MuxDriver::enableNextPort() {
-    int newPortOneBased = ((this->currentPortIndex + 1) % this->numPorts) + 1;
-    this->enablePort(newPortOneBased);
+    int newPortOneBased = ((currentPortIndex + 1) % numPorts) + 1;
+    enablePort(newPortOneBased);
 }
 
 int MuxDriver::enabledPort() {
     // Return value is one-based
-    return this->currentPortIndex + 1;
+    return currentPortIndex + 1;
 }
 
 bool MuxDriver::shouldReset() {
-    return this->_shouldReset;
+    return _shouldReset;
 }
